3a24f357d0c10795902e97e521001b9159bd51a3
Ian Warner <Ian-Warner@users.noreply.github.com>
2016-05-02 21:11:54 -0600

    Following Tutorial
    Did items in the tutorial for Lesson 1 - Data Types up to part 6.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,27 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //$("#main").append("Ian Warner")
+
+//var firstname = "Ian";
+//var age = 35;
+
+//var awesomeThoughts = "I am Ian and I am AWESOME!"
+//console.log(awesomeThoughts);
+
+//var email = "warnerian@gmail.com";
+
+//var newEmail = email.replace("gmail", "selexy");
+
+//console.log(email);
+//console.log(newEmail);
+
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+//$("#main").append(funThoughts);
+var name = "Ian Warner";
+var role = "Full Stack Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
